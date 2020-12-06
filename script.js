ffe2575a8596b34b7c464b22bb972ce868da68b1
gopi-xyz <75554463+gopi-xyz@users.noreply.github.com>
2020-12-05 22:04:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 function loadPlaces(position) {
     const params = {
         radius: 300,    // search places not farther than this value (in meters)
-        clientId: '<your-client-id>',
-        clientSecret: '<your-client-secret>',
+        clientId: 'NEDDOOD5WQNQBTVAVTJNZVQ5QJ3OXHXZUWPOU25HC3EGXJKF',
+        clientSecret: 'KPNVEQUQENFK100243UI5ZZE2IE5KVYCGP5IOXUX1EHAQOEU',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
